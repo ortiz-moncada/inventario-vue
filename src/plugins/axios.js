2b7176b094284a27c01cdabdb7bfa063a527c1ee
santiagodiaz21 <fsantiagodiazc21@gmail.com>
2024-12-03 06:55:48 -0500

    render

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,8 @@ import Axios from 'axios';
 
 const token = JSON.parse(localStorage.getItem('auth'));
 const apiClient = Axios.create({
-    baseURL: 'http://localhost:3200/api',
+   /*  baseURL: 'http://localhost:3200/api', */
+baseURL:'https://proyecto-1-js.onrender.com',
     headers: {
         "x-token": token
     }
